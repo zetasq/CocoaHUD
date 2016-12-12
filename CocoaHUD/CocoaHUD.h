455fc9085c6a2d347cfd8233c11638e297722ec2
Zhu Shengqi <shengqi@teambition.com>
2016-12-12 11:29:14 +0800

    Initial Commit

---
+++ CocoaHUD/CocoaHUD.h
@@ -0,0 +1,19 @@
+//
+//  CocoaHUD.h
+//  CocoaHUD
+//
+//  Created by Zhu Shengqi on 12/12/2016.
+//  Copyright © 2016 zetasq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CocoaHUD.
+FOUNDATION_EXPORT double CocoaHUDVersionNumber;
+
+//! Project version string for CocoaHUD.
+FOUNDATION_EXPORT const unsigned char CocoaHUDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaHUD/PublicHeader.h>
+
+
